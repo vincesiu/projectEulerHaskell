a37e59f89497e088f48afeb74645e45128f7e902
Vincent Siu <vince.siu4@gmail.com>
2016-01-10 01:24:23 -0800

    Finished problem 2

--- problem002.hs
+++ problem002.hs
@@ -1,3 +1,19 @@
 -- Vincent Siu
 -- projectEuler
 -- Problem 002
+
+generateFibonacci i = helper_Fibonacci' [1,1] i 
+                        
+helper_Fibonacci :: [Int]->Int->[Int]
+helper_Fibonacci (a:b:l) n = if n == 0
+                                then l
+                                else helper_Fibonacci ((a+b):a:b:l) (n-1)
+
+helper_Fibonacci' :: [Int]->Int->[Int]
+helper_Fibonacci' (a:b:l) n = if (a + b) > n
+                                then a:b:l
+                                else helper_Fibonacci' ((a+b):a:b:l) n
+
+list = [x | x <- generateFibonacci 4000000, even x]
+
+calculate = sum list
